94c5a50ba0e53fc0e25734663c3bfd095c6479a1
jasilflorez <hans.eguis@wowdesarrollos.com>
2022-10-19 09:13:11 -0500

    mi trabajo de javaScrip

--- inde.js
+++ inde.js
@@ -1,30 +1,42 @@
 
-let i = 0;
+let newContacto="";
 let lista = ["jose", "henry","geral"];
-while (i <= 2) {  
-    const newContacto =prompt("Agregar Nuevo Contactos");
+let seguir =""
+do {
+    do {
+        newContacto = prompt("Agregar Nuevo Contactos Nombre y Apellido");
+        if (newContacto) {
+            add(newContacto,lista);
+            console.log(lista);
     
-
-    add(newContacto,lista)
-    i++
-}
-let lista2 = "";
+        } else {
+            alert("ingresa tu nombre y apellido")
+        }   
+    } while (newContacto == "");
+    
+    
+    let lista2 = "";
 for (let i = 0; i < lista.length; i++) {
-    lista2 += "  "+lista[i]+" ";
+    lista2 += i +"_"+lista[i]+"  ";
     
 }
+let eliminar = prompt("Quiere Eliminar algun Contacto Agregado \n si o no")
+if (eliminar==="si") {
+    const Contacto = prompt("escribe el numero del contacto que quieres eliminar\n"+ lista2 +"\n")
+    
+    delet(Number(Contacto),lista)
+    console.log(lista);
+}else{
+    alert("No Eliminaste A Nadien De Tu Costactos")
+}
 
-let eliminar = alert('Quiere eliminar algun Contacto De Aqui' + lista2)
-console.log(lista);
-const eliminarContacto = prompt("elije cual delos que estaba en orde De Posicion quiere elimar "+" 0: Primero" + " 1: Segundo" + " 2: Tercero"+" 3: Cuarto  " + " 4: quinto "  +  " 5 :sestimo")
 function add(nombre, arr){        
     arr.push(nombre);
 }
 function delet(eliminarContacto,arr){
     arr.splice(eliminarContacto,1)
 }
-delet(eliminarContacto,lista)
-console.log(lista)
+seguir = prompt("Quieres Seguir agregado \n si o no ")
 
-
-
+console.log(lista)    
+} while (seguir === "si");
